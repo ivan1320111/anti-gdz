0eb0e18b5c8ce01ff8d01bb90ebd9763baed4921
ivan1320111 <110672973+ivan1320111@users.noreply.github.com>
2024-12-18 18:47:08 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,11 @@
+const activateButton = document.getElementById("activateButton")
+var activated = 0
+activateButton.addEventListener("click", () => {
+activated = activated + 1
+})
+
+while(activated => 1) {
+    console.log("anti-gdz active")
+    if(window.location.href = "https://gdz.ru")
+        src = "vopros.html"
+}
